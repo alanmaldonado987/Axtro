519da7c7e8b31657ef62ac6f7f39775a4d117833
= <=>
2025-11-21 21:42:13 -0500

    fix: ajuste en color logo

--- axtro-client/src/components/Sidebar.jsx
+++ axtro-client/src/components/Sidebar.jsx
@@ -12,7 +12,7 @@ const Sidebar = ({ isMenuOpen, setIsMenuOpen }) => {
     <div className={`flex flex-col h-screen min-w-72 p-5 dark:bg-gradient-to-b from-[#242124]/30 to-[#000000]/30 border-r border-[#80609F]/30 backdrop-blur-3xl transition-all duration-500 max-md:absolute left-0 z-1 ${!isMenuOpen && 'max-md:-translate-x-full'}`}>
       {/* Logo */}
       <div className='flex items-center'>
-        <img src={theme === 'dark' ? assets.logo_full : assets.logo_full_dark } alt='' className='w-full max-w-22' /> <p className='text-2xl'><i>Axtro AI</i></p>
+        <img src={theme === 'dark' ? assets.logo_full : assets.logo_full_dark } alt='' className='w-full max-w-22' /> <p className='text-2xl dark:text-white'><i>Axtro AI</i></p>
       </div>
 
       {/* New Chat Button */}
